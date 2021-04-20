944a1b8892a7f79bab0808099311f8a4e62f7fd2
Tran Thai Tan <tantan992@gmail.com>
2021-04-20 12:03:58 -0400

    - Init project

---
+++ GitJobs/GitJobs/GitJobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitJobsApp.swift
+//  GitJobs
+//
+//  Created by Tan Tan on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct GitJobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
